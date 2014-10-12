2e18e34c0bb8024dfe8669a3f051c0453c1e63bc
Vinnie Magro <vmagro@socaldevs.com>
2014-10-12 12:41:54 -0700

    Skeleton code for BuoyDetector

---
+++ src/main/java/com/uscauv/vision/BuoyDetector.java
@@ -0,0 +1,30 @@
+package com.uscauv.vision;
+
+import com.uscauv.events.image.ForwardCameraImageEvent;
+import org.opencv.core.Mat;
+
+/**
+ * Analyzes forward images to find buoy.
+ * Algorithm described as follows:
+ * TODO: document algorithm
+ */
+public class BuoyDetector {
+
+    private static BuoyDetector instance;
+
+    public static BuoyDetector getInstance() {
+        if (instance == null) {
+            instance = new BuoyDetector();
+        }
+        return instance;
+    }
+
+    private BuoyDetector() {
+
+    }
+
+    public void onForwardImage(ForwardCameraImageEvent event) {
+        Mat img = event.getImage();
+    }
+
+}
